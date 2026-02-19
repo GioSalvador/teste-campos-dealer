5b87dc00e1b5737537754221039a23df0b5d6a0e
GioSalvador <strokemusic07@gmail.com>
2026-02-19 15:33:53 -0300

    feat: cria página de criação e listagem de produtos

---
+++ TesteCamposDealer.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteCamposDealer.Web.Services;
+using TesteCamposDealer.Web.ViewModels;
+
+namespace TesteCamposDealer.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ProdutoService _produtoService;
+
+        public ProdutoController(ProdutoService produtoService)
+        {
+            _produtoService = produtoService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var produtos = await _produtoService.ObterTodosAsync();
+            return View(produtos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProdutoCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            await _produtoService.CriarAsync(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var produto = await _produtoService.ObterPorIdAsync(id);
+
+            if (produto == null)
+                return NotFound();
+
+            var model = new ProdutoEditViewModel
+            {
+                idProduto = produto.IdProduto,
+                dscProduto = produto.Descricao,
+                preco = produto.Valor
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProdutoEditViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            await _produtoService.AtualizarAsync(id, model);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var produto = await _produtoService.ObterPorIdAsync(id);
+
+            if (produto == null)
+                return NotFound();
+
+            return View(produto);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _produtoService.DeletarAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ TesteCamposDealer.Web/Views/Produto/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TesteCamposDealer.Web.Views.Produto
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
